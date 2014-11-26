a9b91e03a294a9e65ecd1340bc9b6f213ece2a59
fbr1 <fbr1@users.noreply.github.com>
2014-11-26 15:38:58 -0300

    ADD basic table

--- Web/src/ElecDomListing/Listing.java
+++ Web/src/ElecDomListing/Listing.java
@@ -1,11 +1,16 @@
 package ElecDomListing;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Business.ElectroDomesticoLogic;
+import Entities.ElectroDomestico;
 
 /**
  * Servlet implementation class Listing
@@ -25,8 +30,17 @@ public class Listing extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		ArrayList<ElectroDomestico> elecDoms = null;
+		try {
+			elecDoms = new ElectroDomesticoLogic().getAll();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		request.setAttribute("elecDoms", elecDoms);
+
+		getServletContext().getRequestDispatcher("/listado.jsp").forward(request, response);
 	}
 
 	/**
